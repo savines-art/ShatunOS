dd542deb603ec1a9cee8b4d48736c3e8cd22aee3
Egor Savin <ko.artist8664@gmail.com>
2026-09-16 17:49:27 +0700

    Bootloader

---
+++ Makefile
@@ -0,0 +1,35 @@
+# =============================================================================
+# Variables
+
+# Build tools
+NASM = nasm -f bin -dN=0x32000
+
+
+# =============================================================================
+# Tasks
+
+all: clean build test
+
+.tmp/boot.bin: src/hello_world.asm
+	$(NASM) src/hello_world.asm -o .tmp/boot.bin
+
+boot.img: .tmp/boot.bin
+	dd if=/dev/zero of=boot.img bs=512 count=2880
+	dd if=.tmp/boot.bin of=boot.img conv=notrunc
+	dd if=./krivie_vtorogo_poryadka.txt of=boot.img conv=notrunc seek=1
+
+build: boot.img
+
+clean:
+	rm -f *.img
+	rm -rf .tmp
+	mkdir .tmp
+
+test: build
+	qemu-system-i386 -cpu pentium2 -m 1g -fda boot.img -monitor stdio -device VGA
+
+debug: build
+	qemu-system-i386 -cpu pentium2 -m 1g -fda boot.img -monitor stdio -device VGA -s -S &
+	gdb
+
+.PHONY: all build clean test debug
---
+++ src/hello_world.asm
@@ -0,0 +1,74 @@
+[BITS 16]
+[ORG 0x7C00]
+xor ax, ax
+mov ds, ax ;this is for labels
+mov ax, 0x07E0
+mov es, ax ;setting [es : bx] buffer
+
+
+;preparing stack
+cli
+xor ax, ax
+mov ss, ax
+mov sp, 0x7C00
+sti
+
+mov ah, 0x02 ;read sector from drive
+mov al, 0x1 ;number of sectors to read
+mov cl, 2 ;number of first sector
+mov ch, 0 ;number of first cilinder
+mov dh, 0 ;number of first head
+mov bx, 0x0 ;setting [es : bx]
+
+cnt: dd 0 ;counter of bytes
+
+load:
+  int 0x13 ;reading of chs
+  jc print_error ;error if carry
+  add bx, 512 ;adding 512 bytes
+  jnc .no_overflow ;checking if we crossed border of 64 Kb
+  mov ax, es
+  add ax, 0x1000
+  mov es, ax
+  .no_overflow:
+  add dword[cnt], 512 ;increasing counter of read bytes
+  cmp dword[cnt], N ;comparing with N and finishing if >=
+  jae loop
+  mov ah, 0x02
+  mov al, 1
+  inc cl ;changing coordinates
+  cmp cl, 19
+  jne .same_head
+    mov cl, 1
+    inc dh
+    cmp dh, 2
+    jne .same_cilinder
+      inc ch
+      mov dh, 0
+      cmp ch, 80
+      je loop
+    .same_cilinder:
+  .same_head:
+  jmp load
+
+
+
+print_error:
+  mov bx, msg
+  .print_loop:
+    mov al, byte[bx]
+    mov ah, 0x0E
+    int 0x10
+    inc bx
+    cmp al, 0
+    jne .print_loop
+
+
+
+loop:
+  jmp loop
+
+msg: db "Error", 0x0A, 0x0D, 0
+
+times 510-($-$$) db 0
+dw 0xAA55
